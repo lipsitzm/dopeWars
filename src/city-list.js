64238ef43238324b65f22e5c777cdf0b0a699540
Mike <lipsitzm@gmail.com>
2015-05-03 23:54:58 +0100

    get modals working again, simplify city-list, remove jquery dependency

--- src/city-list.js
+++ src/city-list.js
@@ -1,14 +1,9 @@
 import {bindable} from 'aurelia-framework';
 
 export class CityList{
-  @bindable cities = null;
+  @bindable current_city = null;
   @bindable drugs = null;
-  @bindable current_city_index = null;
 
   constructor(){
-  }
-
-  UpdateCurrentCity(idx) {
-    this.current_city_index = idx;
   }
 }
