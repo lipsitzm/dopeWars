3e720ea616644d60f836bdc857222372fe11fe33
Mike <lipsitzm@gmail.com>
2015-05-26 17:43:06 -0400

    updating dist versions post merge

--- dist/city-list.js
+++ dist/city-list.js
@@ -26,7 +26,6 @@ System.register(['aurelia-framework', './services/gameEngineService', './engines
           this.GameEngineService = gameEngineService;
           this.GameEngineService.GetGameEngine().then(function (gameEngine) {
             _this.GameEngine = gameEngine;
-            _this.CurrentCity = _this.GameEngine.Cities[_this.GameEngine.CurrentCityIndex];
           });
         }
 
@@ -37,13 +36,9 @@ System.register(['aurelia-framework', './services/gameEngineService', './engines
           value: undefined,
           enumerable: true
         }, {
-          key: 'CurrentCity',
-          value: undefined,
-          enumerable: true
-        }, {
           key: 'MoveCity',
           value: function MoveCity(index) {
-            this.CurrentCity = this.GameEngine.MoveCity(index);
+            this.GameEngine.MoveCity(index);
           }
         }]);
 
@@ -55,4 +50,4 @@ System.register(['aurelia-framework', './services/gameEngineService', './engines
     }
   };
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNpdHktbGlzdC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO3NGQUthLFFBQVE7Ozs7bUNBTGIsUUFBUTtpQ0FBRSxNQUFNOztxREFDaEIsaUJBQWlCOztzQ0FDakIsVUFBVTs7Ozs7Ozs7O0FBR0wsY0FBUTtBQUlSLGlCQUpBLFFBQVEsQ0FJUCxpQkFBaUIsRUFBRTs7Ozs7QUFDN0IsY0FBSSxDQUFDLGlCQUFpQixHQUFHLGlCQUFpQixDQUFDO0FBQzNDLGNBQUksQ0FBQyxpQkFBaUIsQ0FBQyxhQUFhLEVBQUUsQ0FBQyxJQUFJLENBQ3ZDLFVBQUEsVUFBVSxFQUFJO0FBQ1osa0JBQUssVUFBVSxHQUFHLFVBQVUsQ0FBQztBQUM3QixrQkFBSyxXQUFXLEdBQUcsTUFBSyxVQUFVLENBQUMsTUFBTSxDQUFDLE1BQUssVUFBVSxDQUFDLGdCQUFnQixDQUFDLENBQUM7V0FDN0UsQ0FDSixDQUFDO1NBQ0g7O3dCQVpVLFFBQVE7Ozs7Ozs7Ozs7OztpQkFjWCxrQkFBQyxLQUFLLEVBQUU7QUFDZCxnQkFBSSxDQUFDLFdBQVcsR0FBRyxJQUFJLENBQUMsVUFBVSxDQUFDLFFBQVEsQ0FBQyxLQUFLLENBQUMsQ0FBQztXQUNwRDs7O0FBaEJVLGdCQUFRLEdBRHBCLE1BQU0sQ0FBQyxpQkFBaUIsQ0FBQyxDQUNiLFFBQVEsS0FBUixRQUFRO2VBQVIsUUFBUTs7OzBCQUFSLFFBQVEiLCJmaWxlIjoiY2l0eS1saXN0LmpzIiwic291cmNlUm9vdCI6Ii9zcmMvIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNpdHktbGlzdC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO3NGQUthLFFBQVE7Ozs7bUNBTGIsUUFBUTtpQ0FBRSxNQUFNOztxREFDaEIsaUJBQWlCOztzQ0FDakIsVUFBVTs7Ozs7Ozs7O0FBR0wsY0FBUTtBQUdSLGlCQUhBLFFBQVEsQ0FHUCxpQkFBaUIsRUFBRTs7Ozs7QUFDN0IsY0FBSSxDQUFDLGlCQUFpQixHQUFHLGlCQUFpQixDQUFDO0FBQzNDLGNBQUksQ0FBQyxpQkFBaUIsQ0FBQyxhQUFhLEVBQUUsQ0FBQyxJQUFJLENBQ3ZDLFVBQUEsVUFBVSxFQUFJO0FBQ1osa0JBQUssVUFBVSxHQUFHLFVBQVUsQ0FBQztXQUM5QixDQUNKLENBQUM7U0FDSDs7d0JBVlUsUUFBUTs7Ozs7Ozs7aUJBWVgsa0JBQUMsS0FBSyxFQUFFO0FBQ2QsZ0JBQUksQ0FBQyxVQUFVLENBQUMsUUFBUSxDQUFDLEtBQUssQ0FBQyxDQUFDO1dBQ2pDOzs7QUFkVSxnQkFBUSxHQURwQixNQUFNLENBQUMsaUJBQWlCLENBQUMsQ0FDYixRQUFRLEtBQVIsUUFBUTtlQUFSLFFBQVE7OzswQkFBUixRQUFRIiwiZmlsZSI6ImNpdHktbGlzdC5qcyIsInNvdXJjZVJvb3QiOiIvc3JjLyJ9
