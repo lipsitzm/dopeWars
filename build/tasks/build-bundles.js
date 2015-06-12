34919757ec64c96fa24d0815297ade3f9c62cf2b
Mike <lipsitzm@gmail.com>
2015-06-11 21:48:06 -0600

    file is now generated, served correctly, and used but it's hacky as i'm building it twice to have a dist and non-dist version and only injecting the non-dist version which gets mapped to the dist version

--- build/tasks/build-bundles.js
+++ build/tasks/build-bundles.js
@@ -9,9 +9,17 @@ var bundles = [
   }
 ];
 
+var bundleCmd = 'jspm bundle \'*\' + aurelia-dope-wars/* + aurelia-bootstrapper + aurelia-loader-default + aurelia-templating-binding + aurelia-dependency-injection + aurelia-router + aurelia-templating-resources + aurelia-history-browser + aurelia-templating-router + aurelia-validation + jquery + bootstrap + css + numeral + lipsitzm/aurelia-bs-modal ';
+
+gulp.task('build-dist-bundles',
+  shell.task(
+    bundles.map(function (bundle) {
+      return bundleCmd + ' app-bundle.js --inject';
+    }))
+);
 gulp.task('build-bundles',
   shell.task(
     bundles.map(function (bundle) {
-      return 'jspm bundle \'*\' + aurelia-dope-wars/* + aurelia-bootstrapper + aurelia-http-client + aurelia-dependency-injection + aurelia-router ' + paths.output + 'app-bundle.js --inject';
+      return bundleCmd + paths.output + 'app-bundle.js';
     }))
 );
--- build/tasks/build.js
+++ build/tasks/build.js
@@ -37,7 +37,7 @@ gulp.task('build', function(callback) {
   return runSequence(
     'clean',
     ['build-system', 'build-html'],
-    'build-bundles',
+    ['build-dist-bundles', 'build-bundles'],
     callback
   );
 });
