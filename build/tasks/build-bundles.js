21c78a9c240c8ae904113c556d40472e67ffaede
Mike <lipsitzm@gmail.com>
2015-06-11 21:58:07 -0600

    bundle now has dopeWar's code and is minified

--- build/tasks/build-bundles.js
+++ build/tasks/build-bundles.js
@@ -9,17 +9,17 @@ var bundles = [
   }
 ];
 
-var bundleCmd = 'jspm bundle \'*\' + aurelia-dope-wars/* + aurelia-bootstrapper + aurelia-loader-default + aurelia-templating-binding + aurelia-dependency-injection + aurelia-router + aurelia-templating-resources + aurelia-history-browser + aurelia-templating-router + aurelia-validation + jquery + bootstrap + css + numeral + lipsitzm/aurelia-bs-modal ';
+var bundleCmd = 'jspm bundle main + aurelia-bootstrapper + aurelia-loader-default + aurelia-templating-binding + aurelia-dependency-injection + aurelia-router + aurelia-templating-resources + aurelia-history-browser + aurelia-templating-router + aurelia-validation + jquery + bootstrap + css + numeral + lipsitzm/aurelia-bs-modal ';
 
 gulp.task('build-dist-bundles',
   shell.task(
     bundles.map(function (bundle) {
-      return bundleCmd + ' app-bundle.js --inject';
+      return bundleCmd + ' app-bundle.js --inject --minify';
     }))
 );
 gulp.task('build-bundles',
   shell.task(
     bundles.map(function (bundle) {
-      return bundleCmd + paths.output + 'app-bundle.js';
+      return bundleCmd + paths.output + 'app-bundle.js --minify';
     }))
 );
