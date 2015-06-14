bd59d9c6959698431de57571344000996e5c8526
Mike <lipsitzm@gmail.com>
2015-06-13 23:56:07 -0600

    fixing negative numbers

--- src/converters/currency-format.js
+++ src/converters/currency-format.js
@@ -3,6 +3,6 @@ import numeral from 'numeral';
 // Stolen from http://eisenbergeffect.bluespire.com/binding-with-value-converters-in-aurelia/
 export class CurrencyFormatValueConverter {
   toView(value) {
-    return numeral(value).format('($0,0)');
+    return numeral(value).format('$-0,0');
   }
 }
